7c2ddb4a86e54652105abdf15c90da688a1da240
LYJ <lyj.blessed2@gmail.com>
2024-11-15 16:58:33 +0900

    init commit

---
+++ src/main/java/com/example/scheduler_develop/SchedulerDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduler_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scheduler_develop/SchedulerDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduler_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
